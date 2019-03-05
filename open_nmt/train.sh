e835983bdf97ffc3184e635642d3c502a0b66bc0
jlikhuva <jlikhuva@stanford.edu>
2019-03-05 02:27:09 -0800

    fix gitignore

--- open_nmt/train.sh
+++ open_nmt/train.sh
@@ -2,4 +2,4 @@ python train.py -data data/gigaword/GIGA \
                 -save_model models/giga \
                 -copy_attn \
                 -reuse_copy_attn \
-                -train_steps 1
+                -train_steps 100000

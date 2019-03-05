0628171cddd9e130d0c1f34b46ab010412df0d20
jlikhuva <jlikhuva@stanford.edu>
2019-03-04 20:25:45 -0800

    script

---
+++ open_nmt/train.sh
@@ -0,0 +1,5 @@
+python train.py -data data/gigaword/GIGA \
+                -save_model models/giga \
+                -copy_attn \
+                -reuse_copy_attn \
+                -train_steps 1
